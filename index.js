c20b9909bafaafdca2b735356d171e992ef674b9
Greg Schmitt <gregjschmitt@gmail.com>
2021-12-13 17:04:32 -0500

    commit tree a63c2b57c9bdc3d0664ddd12a5e92aaf6498fe0a

--- index.js
+++ index.js
@@ -1,4 +1,56 @@
-/* Your Code Here */
+function createEmployeeRecord(employee) {
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map(emp => createEmployeeRecord(emp));
+}
+
+function createTimeInEvent(dateStamp) {
+    // dateStamp is in the format: YYYY-MM-DD HHMM
+    this.timeInEvents.push(
+        {
+            type: "TimeIn",
+            hour: parseInt(dateStamp.slice(11, 15), 10),
+            date: dateStamp.slice(0, 10)
+        }
+    )
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    // dateStamp is in the format: YYYY-MM-DD HHMM
+    this.timeOutEvents.push(
+        {
+            type: "TimeOut",
+            hour: parseInt(dateStamp.slice(11, 15), 10),
+            date: dateStamp.slice(0, 10)
+        }
+    )
+    return this;
+}
+
+function hoursWorkedOnDate(dateWorked) {
+    //  console.log("here in hoursWorkedOnDate")
+    const hoursWorked =
+        (this.timeOutEvents.find(event => event.date === dateWorked).hour
+            - this.timeInEvents.find(event => event.date === dateWorked).hour
+        ) / 100;
+    return hoursWorked;
+}
+
+function wagesEarnedOnDate(dateWorked) {
+    //  console.log("here in wagesWorkedOnDate")
+    const hoursWorked = hoursWorkedOnDate.bind(this);
+    return hoursWorked(dateWorked) * this.payPerHour;
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +72,44 @@ const allWagesFor = function () {
 
     return payable
 }
+
+function calculatePayroll(employees) {
+    return (
+        employees.reduce((total, employee) => {
+            const allWages = allWagesFor.bind(employee);
+            total += allWages();
+            return total;
+        }, 0)
+    )
+}
+
+function findEmployeeByFirstName(employees, firstName) {
+    return employees.find(emp => emp.firstName === firstName);
+}
+
+// let emps = createEmployeeRecords([
+//     ["Greg", "Schmitt", "Engineer", 200],
+//     ["Mario", "Jones", "Programmer", 100],
+//     ["Marcos", "Gray", "Clerk", 25]]
+// );
+
+// const createTimeIn = createTimeInEvent.bind(emps[0])
+
+// createTimeIn("2021-12-13 1400");
+// createTimeIn("2021-12-14 1400");
+// createTimeIn("2021-12-15 1400");
+// createTimeIn("2021-12-16 1400");
+
+// const createTimeOut = createTimeOutEvent.bind(emps[0])
+// createTimeOut("2021-12-13 1600");
+// createTimeOut("2021-12-14 1600");
+// createTimeOut("2021-12-15 1600");
+// createTimeOut("2021-12-16 1600");
+
+// const hoursWorked = hoursWorkedOnDate.bind(emps[0]);
+// console.log("hours worked: ", hoursWorked("2021-12-15"))
+
+// const wagesEarned = wagesEarnedOnDate.bind(emps[0]);
+// console.log("wages earned: ", wagesEarned("2021-12-15"));
+
 
